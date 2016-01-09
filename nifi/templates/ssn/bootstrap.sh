9c8c40d0b3c47de2cc3b8b211ff2ee1be4748ff4
Artem Ervits <generic13@gmail.com>
2016-01-08 20:41:17 -0500

    added nifi setup

---
+++ nifi/templates/ssn/bootstrap.sh
@@ -0,0 +1,17 @@
+# create directories for local FileSystem
+mkdir -p /tmp/csv /tmp/ssn
+
+# create a destination directory on HDFS
+sudo -u hdfs hdfs dfs -mkdir /tmp/nossn
+sudo -u hdfs hdfs dfs -chmod -R 777 /tmp/nossn
+
+# download the latest HDF release
+wget http://s3.amazonaws.com/public-repo-1.hortonworks.com/HDF/1.1.1.0/nifi-1.1.1.0-12-bin.tar.gz
+tar xvzf nifi-1.1.1.0-12-bin.tar.gz && cd nifi-1.1.1.0-12/conf
+
+# replace nifi port 8080 to 9090
+sed 's/9090/8080/' nifi.properties > new
+mv new nifi.properties
+
+# start nifi
+../bin/nifi.sh start
