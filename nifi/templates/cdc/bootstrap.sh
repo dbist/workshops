e4fc24ba091de7f4038ef2960bb5c5db6b876593
Artem Ervits <generic13@gmail.com>
2016-04-18 13:40:05 -0400

    updated for Nifi 0.6.1/HDF 1.2

--- nifi/templates/cdc/bootstrap.sh
+++ nifi/templates/cdc/bootstrap.sh
@@ -10,7 +10,8 @@ HDF_VERSION_SHORT=1.2.0.0
 
 # download the latest HDF release
 wget http://s3.amazonaws.com/public-repo-1.hortonworks.com/HDF/centos6/1.x/updates/$HDF_VERSION_SHORT/$HDF_VERSION.tar.gz
-# tar xvzf HDF-$HDF_VERSION-bin.tar.gz && cd $HDF_VERSION/conf
+mv $HDF_VERSION.tar.gz ~/
+tar xvzf ~/$HDF_VERSION.tar.gz && cd ~/$HDF_VERSION/conf
 
 # replace nifi port 8080 to 9090
 sed 's/8080/9090/' nifi.properties > new
