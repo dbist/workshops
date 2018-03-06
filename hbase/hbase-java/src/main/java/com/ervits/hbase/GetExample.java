0dbf37340e2c04485088150023a7d0b7bd810f18
Artem Ervits <generic13@gmail.com>
2018-03-06 16:22:48 -0500

    get example

---
+++ hbase/hbase-java/src/main/java/com/ervits/hbase/GetExample.java
@@ -0,0 +1,33 @@
+package com.ervits.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Result;
+
+public class GetExample {
+
+    public static void main(String[] args) throws IOException {
+        Configuration conf = HBaseConfiguration.create();
+
+        // conf.set("hbase.zookeeper.quorum", "sandbox.hortonworks.com");
+        // conf.set("hbase.zookeeper.property.clientPort", "2181");
+        // conf.set("zookeeper.znode.parent", "/hbase-unsecure");
+        try (Connection connection = ConnectionFactory.createConnection(conf);
+                Table table = connection.getTable(TableName.valueOf("wiki"))) {
+
+            Get get = new Get(Bytes.toBytes("row1"));
+            Result r = table.get(get);
+            byte[] b = r.getValue("title".getBytes(), "attr".getBytes());
+
+            System.out.println(b);
+        }
+    }
+}
