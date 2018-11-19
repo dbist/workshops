a9d0548a4d7ae7e233e1b746cd337b62d1fa557d
Artem Ervits <generic13@gmail.com>
2018-11-19 14:59:00 -0500

    new java example

---
+++ spark/java/src/main/java/org/apache/spark/examples/MyExample.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.apache.spark.examples;
+
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import static org.apache.spark.sql.functions.col;
+import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import org.apache.spark.sql.AnalysisException;
+
+public class MyExample {
+
+    //https://spark.apache.org/docs/2.3.2/sql-programming-guide.html#datasets-and-dataframes
+    public static void main(String[] args) throws IOException, AnalysisException {
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL basic example")
+                .config("spark.some.config.option", "some-value")
+                .getOrCreate();
+
+        Dataset<Row> df = spark.read().json(args[0]);
+// Displays the content of the DataFrame to stdout
+        df.show();
+// +----+-------+
+// | age|   name|
+// +----+-------+
+// |null|Michael|
+// |  30|   Andy|
+// |  19| Justin|
+// +----+-------+
+// Print the schema in a tree format
+        df.printSchema();
+// root
+// |-- age: long (nullable = true)
+// |-- name: string (nullable = true)
+
+// Select only the "name" column
+        df.select("name").show();
+// +-------+
+// |   name|
+// +-------+
+// |Michael|
+// |   Andy|
+// | Justin|
+// +-------+
+
+// Select everybody, but increment the age by 1
+        df.select(col("name"), col("age").plus(1)).show();
+// +-------+---------+
+// |   name|(age + 1)|
+// +-------+---------+
+// |Michael|     null|
+// |   Andy|       31|
+// | Justin|       20|
+// +-------+---------+
+
+// Select people older than 21
+        df.filter(col("age").gt(21)).show();
+// +---+----+
+// |age|name|
+// +---+----+
+// | 30|Andy|
+// +---+----+
+
+// Count people by age
+        df.groupBy("age").count().show();
+// +----+-----+
+// | age|count|
+// +----+-----+
+// |  19|    1|
+// |null|    1|
+// |  30|    1|
+// +----+-----+
+// Register the DataFrame as a SQL temporary view
+        df.createOrReplaceTempView("people");
+
+        Dataset<Row> sqlDF = spark.sql("SELECT * FROM people");
+        sqlDF.show();
+
+// +----+-------+
+// | age|   name|
+// +----+-------+
+// |null|Michael|
+// |  30|   Andy|
+// |  19| Justin|
+// +----+-------+
+// Register the DataFrame as a global temporary view
+        df.createGlobalTempView("people");
+
+// Global temporary view is tied to a system preserved database `global_temp`
+        spark.sql("SELECT * FROM global_temp.people").show();
+// +----+-------+
+// | age|   name|
+// +----+-------+
+// |null|Michael|
+// |  30|   Andy|
+// |  19| Justin|
+// +----+-------+
+
+// Global temporary view is cross-session
+        spark.newSession().sql("SELECT * FROM global_temp.people").show();
+// +----+-------+
+// | age|   name|
+// +----+-------+
+// |null|Michael|
+// |  30|   Andy|
+// |  19| Justin|
+// +----+-------+
+
+        // save output to table
+        sqlDF.write().bucketBy(42, "name").sortBy("age").saveAsTable("people_bucketed");
+    }
+}
